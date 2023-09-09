4fc863dbab0b85d2ac21ce39982b23b9e300e945
avani-samaga <avani@slow.studio>
2023-09-09 19:19:23 +0530

    add sheep movement as function
    arranged setdesination, added sheepPos and moveSheep functions. now sheep doesn't move anymore

--- Assets/sheepMovement.cs
+++ Assets/sheepMovement.cs
@@ -13,55 +13,63 @@ public class sheepMovement : MonoBehaviour
     float minimumDistance = 0.1f;
     float sheepMoveDistance = 1.0f; // this is how much the sheep moves when asked to
 
-    private void Update() {
+    private void Update()
+    {
 
         /* when click occurs, set a destination for the sheep */
 
         // check when click occurs
-        if (Input.GetMouseButtonDown(0)) { // "0":"Pressed left-click." 
+        if (Input.GetMouseButtonDown(0))
+        { // "0":"Pressed left-click." 
             // get tap-position
             tap = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //get sheep's currnet position
+            getSheepPos();
             // set destination
             setDestination();
             // instruct sheep that it can move
             moving = true;
-        }
-
-        void setDestination() {
-            // get sheep's position
-            sheepPos = new Vector2 (   GetComponent<Transform>().position.x, 
-                                    GetComponent<Transform>().position.y 
-                                );
-            // Debug.Log(GetComponent<Transform>().position);
-
-            // set goal
-            goal = (tap - sheepPos).normalized //direction
-                    * (tap - sheepPos).magnitude //magnitude
-                    ;
-            Debug.Log(goal);
-        }
-
-        /* sheep movement */
-
-        // if the sheep is allowed to move 
-
-        if (moving) {
-
+            // make sheep move
             /*  if the sheep is still far from click-position */
-            
+            if (moving && (goal - sheepPos).magnitude >= minimumDistance)
+            {
+                moveSheep();
+            }
+            // else sheep is not allowed to move
+            else moving = false;
+
+            void setDestination()
+            {
+                // set goal
+                goal = (tap - sheepPos).normalized //direction
+                        * sheepMoveDistance //magnitude
+                        ;
+                // Debug.Log(GetComponent<Transform>().position);
+                Debug.Log(goal);
+            }
+
+            void getSheepPos()
+            {
+                // get sheep's position
+                sheepPos = new Vector2(GetComponent<Transform>().position.x,
+                                        GetComponent<Transform>().position.y
+                                    );
+            }
+
+            /* sheep movement */
+
             // print the distance between sheep and click
             // Debug.Log( "distance between sheep and its goal = " + (goal - sheepPos).magnitude );
 
             // if the sheep is far enough, it can move toward the goal
-            if ((goal - sheepPos).magnitude >= minimumDistance) {
+            void moveSheep()
+            {
                 // set speed of movement
                 // (make sure the sheep moves at the speed of the device-based on fps)
                 float step = speed * Time.deltaTime;
                 // let the sheep move
                 transform.position = Vector2.MoveTowards(transform.position, goal, step);
             }
-            // else: sheep is instructed that it can not move
-            else moving = false;
         }
     }
 }
