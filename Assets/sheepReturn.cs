ebab089dda924879466ac706b40bb9c6ef22b60f
shobhan-ss <86220370+shobhan-ss@users.noreply.github.com>
2023-09-19 20:45:55 +0900

    Revert "add system onBushClick"
    This reverts commit 123d639ecc8eeaf2561b3461062d905c984b0b03.

--- Assets/sheepReturn.cs
+++ Assets/sheepReturn.cs
@@ -1,43 +1,32 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SheepReturn : MonoBehaviour
+public class sheepReturn : MonoBehaviour
 {
-    float speed = 8f;
-    private Vector2 bush;
-
-    SheepReturn sheepReturn;
+    public float speed = 8f;
     private void Start()
     {
+        gameEvents.current.onBushClick += goBack;
     }
 
-    private void Update()
+    private void goBack()
     {
-
-        // makes teh sheep return to the bush 
-        Debug.Log("sheep recieved order to go back");
-        float step = speed * Time.deltaTime;
-
+        //find bush position
+        //getBushPos();
         //Vector2 bushPos = getBushPos();
-        bush = new Vector2(0.0f, 0.0f);
-        transform.position = Vector2.MoveTowards(transform.position, bush, step);
-        //transform.position = Vector2.MoveTowards(transform.position, bush, step);
+        Vector2 back = GameObject.Find("bush").transform.position.normalized;
+        float step = speed * Time.deltaTime;
+        // let the sheep move
+        transform.position = Vector2.MoveTowards(transform.position, back, step);
     }
 
-
-    // private Vector2 getBushPos()
+   // Vector2 getBushPos()
     //{
-        //get the Input from Horizontal axis
-        //float horizontalInput = Input.GetAxis("Horizontal");
-        //get the Input from Vertical axis
-        //float verticalInput = Input.GetAxis("Vertical");
+      //  return new Vector2(
+      //                          GetComponent<Transform>().position.x,
+       //                         GetComponent<Transform>().position.y
+       //                         );
+   // }
 
-       //float step = speed * Time.deltaTime;
-
-       // Vector2 direction = new Vector2(horizontalInput * step, verticalInput * step);
-
-    }
-       
-
+}
