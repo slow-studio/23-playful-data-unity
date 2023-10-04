8cf485f02e49758fae7a8fecb0c1497f57f2cfd8
avani-samaga <avani@slow.studio>
2023-10-04 11:03:55 +0530

    ongoing changes
    - sheep turns red when clicked (completed)
    - sheep peeks out (incomplete ; broken code)

--- Assets/delegateFunctions/delegateMeat.cs
+++ Assets/delegateFunctions/delegateMeat.cs
@@ -5,17 +5,45 @@ using UnityEngine;
 public class delegateMeat : MonoBehaviour
 {
     public delegate void meatDelegate();
-    public delegate bool ifMeat(int i);
+    //public delegate bool ifMeat(int i);
 
-    private meatDelegate meatDelegateFunction;
-    private ifMeat ifMeatFunction;
+    meatDelegate meatDelegateFunction;
+    //ifMeat ifMeatFunction;
 
     private void Start()
     {
         //lambda expression for compactly defining a function
         //meatDelegateFunction = () => { Debug.Log(" testing testing")};
 
-        ifMeatFunction = (int i) => { return i < 5; };
-        Debug.Log(ifMeatFunction(1));
+        //ifMeatFunction = (int i) => { return i < 5; };
+        //Debug.Log(ifMeatFunction(1));
     }
+
+    void OnMouseDown()
+    {
+        //calling the delegates to turn the sheep to meat
+        meatDelegateFunction += turnMeat;
+        meatDelegateFunction += calledMeat;
+
+        if (meatDelegateFunction != null)
+        {
+            meatDelegateFunction();
+        }
+    }
+
+    void calledMeat()
+    {
+        print("sheep will now become red");
+    }
+
+    void turnMeat()
+    {
+        //sheep turns red if it is currently white
+        if (GetComponent<Renderer>().material.color == Color.white)
+            GetComponent<Renderer>().material.color = Color.red;
+
+        else
+            GetComponent<Renderer>().material.color = Color.white;
+    }
+
 }
---
+++ Assets/delegateFunctions/sheepPeek.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sheepPeek : MonoBehaviour
+{
+    sheepMovement sheepPeek;
+    //[SerializeField] - can see it in the other scripts but can't be edited like in public declaration
+    [SerializeField] GameObject sheep;
+
+    // awake is called before start. Start is called before the first frame update
+    void Awake()
+    {
+        //finding the gameobject refereced by the script sheepmovement
+        sheepMovement = GameObject.Find("sheep").GetComponent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
