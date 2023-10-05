32b1fecbba37661a782951df19043f176b9c47c4
avani-samaga <avani@slow.studio>
2023-10-05 14:47:53 +0530

    added emitter and listener
    testing emitter and listener on right click event

---
+++ Assets/eventScripts/TestListener.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TestListener : MonoBehaviour
+{
+    private UnityAction onTest;
+    private void Awake()
+    {
+        onTest = new UnityAction(OnTest);
+    }
+    private void OnEnable()
+    {
+        EventManager.StartListening("Test", onTest);
+    }
+    private void OnDisable()
+    {
+        EventManager.StopListening("Test", onTest);
+    }
+    private void OnTest()
+    {
+        Debug.Log("recieved test :D");
+    }
+}
