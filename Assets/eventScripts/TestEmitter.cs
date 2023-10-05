7addd95c29af571127f2702af7feba8124410c41
avani-samaga <avani@slow.studio>
2023-10-05 14:56:30 +0530

    working test eventManager
    tested event manager with emitters and listeners, working.

--- Assets/eventScripts/TestEmitter.cs
+++ Assets/eventScripts/TestEmitter.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TestEmitter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //this emitter code is to trigger events
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //when mouse button is clicked, the event qill be triggered
+        //recievers will now react to this with a callback function
+        if (Input.GetMouseButtonDown(1))
+            EventManager.TriggerEvent("Test");
     }
 }
