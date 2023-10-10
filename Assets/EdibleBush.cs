54e596e43bb5cfa881829db80842fe2c0f1354a0
avani-samaga <avani@slow.studio>
2023-10-10 10:52:13 +0530

    test destroyable object

--- Assets/EdibleBush.cs
+++ Assets/EdibleBush.cs
@@ -8,7 +8,27 @@ public class EdibleBush : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //creating and naming game object
+        GameObject newGameObject = new GameObject("empty gameobject");
+        //setting the position
+        newGameObject.transform.position = new Vector2(5, 6);
+        //setting parent
+        newGameObject.transform.parent = transform;
+
+        //getting a new prefab
+        //creating, gtting position and parent
+        GameObject prefabGObject = Instantiate(
+            prefab,
+            new Vector3(1, 1, 1),
+            // quaternion creates a blank, empty set of rotation values to use on the prefab
+            Quaternion.identity,
+            transform
+            );
+        //naming
+        prefabGObject.name = "prefab game object";
+
+        Destroy(newGameObject, 2.0f);
+        Destroy(prefabGObject, 4.0f);
     }
 
 }
