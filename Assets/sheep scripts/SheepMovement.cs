44957f015b27c19b88ffdda2b83495d9895c4b26
avani-samaga <avani@slow.studio>
2023-10-10 15:06:54 +0530

    attempt smoothen moevemnt code

--- Assets/sheep scripts/SheepMovement.cs
+++ Assets/sheep scripts/SheepMovement.cs
@@ -12,7 +12,12 @@ public class SheepMovement : MonoBehaviour
     float minimumDistance = 0.1f;
     float sheepMoveDistance = 2f; // this is how much the sheep moves when asked to
     float sheepJitter = 0.25f; // when the sheep moves, it moves a bit randomly by this value
+    private Rigidbody2D body;
 
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
 
@@ -29,72 +34,76 @@ public class SheepMovement : MonoBehaviour
             setDestination();
             // instruct sheep that it can move
             moving = true;
+            if (moving)
+                moveSheep();
         }
 
-        void setDestination() {
+    }
+    void setDestination() {
 
-            // store the sheep's current position
-            Vector2 sheepPos = getSheepPos();
+        // store the sheep's current position
+        Vector2 sheepPos = getSheepPos();
 
-            // this is the vector joining the sheep to the tap
-            Vector2 sheepToTap = new Vector2(
-                                (tap.x - sheepPos.x) // the vector.x between sheep and tap
-                                    + (sheepJitter*Random.value) // add jitter
-                                ,
-                                (tap.y - sheepPos.y) // the vector.y between sheep and tap
-                                    + (sheepJitter*Random.value) // add jitter
-                                );
+        // this is the vector joining the sheep to the tap
+        Vector2 sheepToTap = new Vector2(
+                            (tap.x - sheepPos.x) // the vector.x between sheep and tap
+                                + (sheepJitter * Random.value) // add jitter
+                            ,
+                            (tap.y - sheepPos.y) // the vector.y between sheep and tap
+                                + (sheepJitter * Random.value) // add jitter
+                            );
 
-            // set goal for the sheep
-            goal = sheepPos // the sheep starts from from where it currently stands
-                    +   (
-                            sheepToTap.normalized // starts moving in the direction of the tap
-                            * 
-                            // it would normally move "sheepMoveDistance", 
-                            // but this Mathf.Min check ensures that it moves 
-                            // less if the sheep is really close to the tap 
-                            // (i.e., tap was closer than the sheep's usual sheepMoveDistance)
-                            Mathf.Min(sheepToTap.magnitude, sheepMoveDistance) 
-                        )
-                    ;
-        }
-
-        /* sheep movement */
-
-        // if the sheep is allowed to move 
-
-        if (moving) {
-
-            // store the sheep's current position
-            Vector2 sheepPos = getSheepPos();
-
-            // print the distance between sheep and click
-            Debug.Log( "distance between sheep and its goal = " + ( goal - sheepPos ).magnitude );
-
-            /*  if the sheep is still far from click-position */
-
-            // if the sheep is far enough, it can move toward the goal
-            if ((goal - sheepPos).magnitude >= minimumDistance) {
-                // set speed of movement
-                // (make sure the sheep moves at the speed of the device-based on fps)
-                float step = speed * Time.deltaTime;
-                // let the sheep move
-                transform.position = Vector2.MoveTowards(transform.position, goal, step);
-            }
-            // else: sheep is instructed that it can not move
-            else moving = false;
-        }
-
-        Vector2 getSheepPos() {
-            return new Vector2 ( 
-                                GetComponent<Transform>().position.x, 
-                                GetComponent<Transform>().position.y 
-                                );
-        }
+        // set goal for the sheep
+        goal = sheepPos // the sheep starts from from where it currently stands
+                + (
+                        sheepToTap.normalized // starts moving in the direction of the tap
+                        *
+                        // it would normally move "sheepMoveDistance", 
+                        // but this Mathf.Min check ensures that it moves 
+                        // less if the sheep is really close to the tap 
+                        // (i.e., tap was closer than the sheep's usual sheepMoveDistance)
+                        Mathf.Min(sheepToTap.magnitude, sheepMoveDistance)
+                    )
+                ;
     }
 
-    private void OnCollisionEnter2D(Collision collision)
+
+    Vector2 getSheepPos() {
+        return new Vector2(
+                            GetComponent<Transform>().position.x,
+                            GetComponent<Transform>().position.y
+                            );
+    }
+
+    /* sheep movement */
+    void moveSheep()
     {
-        moving = false;
+        // if the sheep is allowed to move 
+        // store the sheep's current position
+        Vector2 sheepPos = getSheepPos();
+
+        // print the distance between sheep and click
+        Debug.Log("distance between sheep and its goal = " + (goal - sheepPos).magnitude);
+
+        /*  if the sheep is still far from click-position */
+
+        // if the sheep is far enough, it can move toward the goal
+        if ((goal - sheepPos).magnitude >= minimumDistance) {
+            // set speed of movement
+            // (make sure the sheep moves at the speed of the device-based on fps)
+            float step = speed * Time.deltaTime;
+            // let the sheep move
+            body.MovePosition(Vector2.MoveTowards(transform.position, goal, step));
+        }
+        // else: sheep is instructed that it can not move
+        else moving = false;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("edibleBush") == true)
+            moving = false;
     }
 }
+
