12813cfdb8df40633ebbc346e350b12877749e79
avani-samaga <avani@slow.studio>
2023-10-11 13:19:22 +0530

    created bush gets script as well
    all clone bushes have getseaten script attached

--- Assets/bushInteractions/SpawnBush.cs
+++ Assets/bushInteractions/SpawnBush.cs
@@ -13,6 +13,7 @@ public class SpawnBush : MonoBehaviour
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             GameObject bush = Instantiate(edibleBush, new Vector3(mousePos.x, mousePos.y, 0), Quaternion.identity);
             bush.AddComponent<CircleCollider2D>();
+            bush.AddComponent<GetsEaten>();
             bush.tag = "edibleBush";
         }
     }
